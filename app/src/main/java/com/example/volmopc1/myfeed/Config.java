f351043e0a50bc6f8dcc1d33735008a0d6dfa173
jituumakanta <jitu.umakanta@gmail.com>
2017-01-16 17:58:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/volmopc1/myfeed/Config.java
@@ -0,0 +1,11 @@
+package com.example.volmopc1.myfeed;
+
+public class Config {
+    //Data URL
+    public static final String DATA_URL = "http://ipvworld.com/hellohelps/feed.php?page=";
+
+    //JSON TAGS
+    public static final String TAG_IMAGE_URL = "image";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_PUBLISHER = "publisher";
+}
